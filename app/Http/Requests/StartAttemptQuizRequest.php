ff39b6d4209cfe1f457e903be530bbb24a46c0ae
Fahridanaa <fahridanaaa@gmail.com>
2025-12-17 10:34:37 +0700

    feat: update authorization methods in request classes to allow access

--- app/Http/Requests/StartAttemptQuizRequest.php
+++ app/Http/Requests/StartAttemptQuizRequest.php
@@ -11,7 +11,7 @@ class StartAttemptQuizRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/SubmitAssignmentRequest.php
+++ app/Http/Requests/SubmitAssignmentRequest.php
@@ -11,7 +11,7 @@ class SubmitAssignmentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
