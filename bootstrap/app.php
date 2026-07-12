52302ed058508a015ecb99ff3a7f17603951638d
Fahridanaa <fahridanaaa@gmail.com>
2026-07-12 23:25:14 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -33,6 +34,13 @@ return Application::configure(basePath: dirname(__DIR__))
             ], 404);
         });
 
+
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => ResponseMessage::NOT_FOUND,
+            ], 404);
+        });
         $exceptions->render(function (ValidationException $e) {
             return response()->json([
                 'success' => false,
